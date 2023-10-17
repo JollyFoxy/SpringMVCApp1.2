e97bf534b750073af8eb1738d5baa6802aca025a
JollyFoxy <91303971+JollyFoxy@users.noreply.github.com>
2023-10-17 17:48:51 +0300

    Работает и слава богу

--- src/main/java/obuch/spring/HelloController.java
+++ src/main/java/obuch/spring/HelloController.java
@@ -1,2 +1,12 @@
-package obuch.spring;public class HelloController {
+package obuch.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController{
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
 }
--- src/main/java/obuch/spring/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/obuch/spring/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,2 +1,20 @@
-package obuch.spring.config;public class MySpringMvcDispatcherSerlvetIntitializer {
+package obuch.spring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
