7cf0335194bf0962c639799a2eb252cd8b085da3
JollyFoxy <91303971+JollyFoxy@users.noreply.github.com>
2023-10-23 16:38:11 +0300

    Работает и слава богу 4

--- src/main/java/obuch/spring/controllers/FirstController.java
+++ src/main/java/obuch/spring/controllers/FirstController.java
@@ -1,11 +1,13 @@
 package obuch.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/first")
@@ -13,15 +15,44 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String  helloPage(@RequestParam(value = "name", required = false)String name,
-                             @RequestParam(value = "surname", required = false)String surname){
+                             @RequestParam(value = "surname", required = false)String surname,
+                             Model model){
 
-        System.out.print("Hello" + " " +name+ " " + surname);
+        model.addAttribute("massage", "Hello" + " " +name+ " " + surname + "\n");
+
+
 
         return "first/hello";
     }
     @GetMapping("/goodbye")
     public String  goodByePage(){
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculation")
+    public String Calculation (@RequestParam(value = "a")int a,
+                               @RequestParam(value = "b")int b,
+                               @RequestParam(value = "operation")String op,
+                               Model model){
+        double r = 0;
+
+        switch (op){
+            case "del":
+                r = a/(double)b;
+                break;
+            case "raz":
+                r = a-b;
+                break;
+            case "umn":
+                r = a*b;
+                break;
+            case "sum":
+                r = a+b;
+                break;
+        }
+
+        model.addAttribute("result", "Result: "+r);
+        return "first/calculation";
     }
 
 }
